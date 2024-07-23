c917acf407640160fa6f983c12507ae56c30b385
RivkaYagelnik <rivk727@gmail.com>
2024-07-24 01:36:25 +0300

    many changes all the project

---
+++ Controllers/PictureController.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DAL;
+using BL;
+using BL.Interfaces;
+using BL.Services;
+using DAL.DTO;
+
+namespace Image_Encryption.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PictureController : Controller
+    {
+        private readonly IPictureService _IPictureService;
+        public PictureController(IPictureService IPictureService)
+        {
+            _IPictureService = IPictureService;
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<bool>> AddPicture([FromQuery] string inputPath, [FromQuery] string message, [FromQuery] int id, [FromQuery] byte[]? key = null, [FromQuery] byte[]? iv = null)
+        {
+            try
+            {
+                Task<bool> res;
+                if (key == null)
+                {
+                    res = _IPictureService.AddPicture(inputPath, message, DateTime.Now, id);
+                }
+                else
+                {
+                    res = _IPictureService.AddPicture(inputPath, message, key, iv, DateTime.Now, id);
+                }
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (ex)
+                return StatusCode(500, "An error occurred while adding the picture.");
+            }
+        }
+        //[HttpPost]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        //public async Task<ActionResult<bool>> AddPicture([FromQuery] string inputPath, [FromQuery] string message, [FromQuery] int id, [FromQuery] byte[]? key = null, [FromQuery] byte[]? iv = null)
+        //{
+        //    try
+        //    {
+        //        Task<bool> res;
+        //        if (key == null)
+        //        {
+        //            res = _IPictureService.AddPicture(inputPath, message, DateTime.Now, id);
+        //        }
+        //        else
+        //        {
+        //            res = _IPictureService.AddPicture(inputPath, message, key, iv, DateTime.Now, id);
+        //        }
+        //        return Ok(await res);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Log the exception (ex)
+        //        Console.WriteLine(ex.Message);
+        //        Console.WriteLine(ex.StackTrace);
+        //        return StatusCode(500, "An error occurred while adding the picture.");
+        //    }
+        //}
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<bool>> DeletePicture(int id)
+        {
+            try
+            {
+                var res = await _IPictureService.DeletePicture(id);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+        }
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<PictureDto>> GetPictureById(int id)
+        {
+            try
+            {
+                var res = await _IPictureService.GetPictureById(id);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"{ex.Message}"); 
+            }
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ICollection<PictureDto>>> GetAllPictures()
+        {
+            try
+            {
+                var res = await _IPictureService.GetAllPictures();
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+        }
+        [HttpGet("DecryptMessageFromImage")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<string>> DecryptMessageFromImage([FromQuery]int id,[FromQuery] byte[] key, [FromQuery]byte[] iv)
+        {
+            try
+            {
+                var res= await _IPictureService.DecryptMessageFromImage(id, key, iv);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DAL;
+using BL;
+using BL.Interfaces;
+using DAL.DTO;
+using BL.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Image_Encryption.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IUserService _IUserService;
+
+        public UserController(IUserService userService)
+        {
+            _IUserService = userService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddUser([FromBody] UserDto newUser)
+        {
+            try
+            {
+                var res = await _IUserService.AddUser(newUser);
+                if (res)
+                    return Ok(res);
+                return BadRequest(res);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (ex)
+                return StatusCode(500, "An error occurred while adding the user.");
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser([FromBody] UserDto user)
+        {
+            try
+            {
+                var res = await _IUserService.UpdateUser(user);
+                if (res)
+                    return Ok(res);
+                return BadRequest(res);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (ex)
+                return StatusCode(500, "An error occurred while updating the user.");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDto>> GetUserById(int id)
+        {
+            try
+            {
+                var user = await _IUserService.GetUserById(id);
+                if (user == null)
+                    return NotFound("User not found.");
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (ex)
+                return StatusCode(500, "An error occurred while retrieving the user.");
+            }
+        }
+
+        [HttpGet("GetAllUsers")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ICollection<UserDto>>> GetAllUsers()
+        {
+            try
+            {
+                var users = await _IUserService.GetAllUsers();
+                if (users == null || users.Count == 0)
+                {
+                    return NotFound("No users found.");
+                }
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (ex)
+                return StatusCode(500, "An error occurred while retrieving users.");
+            }
+        }
+
+        [HttpPut("ResetPassword")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> ResetPassword([FromQuery] string password1, [FromQuery] string password2, [FromQuery] int id, [FromQuery] string validation = " ")
+        {
+            try
+            {
+                if (password1 != password2)
+                {
+                    return BadRequest("Passwords do not match.");
+                }
+
+                // Validate user ID and perform the password reset
+
+                // Perform the password reset operation
+                bool result = await _IUserService.ResetPassword(password1, password2, id, validation);
+
+                if (result)
+                {
+                    return Ok("Password has been reset successfully.");
+                }
+                else
+                {
+                    return StatusCode(500, "An error occurred while resetting the password.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (ex)
+                return StatusCode(500, "An error occurred while resetting the password.");
+            }
+        }
+    }
+}
